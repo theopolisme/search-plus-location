d398633be5d50e53d82bf83496b3225a56c3a969
Theopolisme <theo@theopatt.com>
2015-07-06 10:21:55 -0500

    Log analytics event if available

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -15,13 +15,24 @@
   // https://github.com/zilverline/react-tap-event-plugin
   injectTapEventPlugin();
 
+  // Log analytics event if available
+  function analytics(state, options={}) {
+    if (!window.ga) {
+      return console.log(`Would be logging pageview: ${state.path}`);
+    }
+
+    options.page = state.path;
+    ga('send', 'pageview', options);
+  }
+
   Router
     .create({
       routes: AppRoutes,
       scrollBehavior: Router.ScrollToTopBehavior
     })
-    .run(function (Handler) {
+    .run(function (Handler, state) {
       React.render(<Handler/>, document.getElementById('app'));
+      analytics(state);
     });
 
 })();
